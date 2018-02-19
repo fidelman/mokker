51d1a0d15a17e6e0b3753704979c2db542935a9e
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-02-19 11:09:56 +0100

    Add eslint

--- test/test.js
+++ test/test.js
@@ -1,61 +1,62 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const should = chai.should();
+
+const should = chai.should(); // eslint-disable-line
 const baseUrl = 'http://localhost:3000';
 
 chai.use(chaiHttp);
 
-describe('Test the Rest server', () => {
-    it('simple GET server', (done) => {
-        chai.request(baseUrl)
-            .get('/test/get')
-            .end((err, res) => {
-                res.should.have.status(200);
-                chai.assert.deepEqual(res.body, { "simple-json": true });
-                done();
-            });
-    });
+describe('Test the Rest server', () => { // eslint-disable-line
+  it('simple GET server', (done) => { // eslint-disable-line
+    chai.request(baseUrl)
+      .get('/test/get')
+      .end((err, res) => {
+        res.should.have.status(200);
+        chai.assert.deepEqual(res.body, { 'simple-json': true });
+        done();
+      });
+  });
 
-    it('GET condition @x=1', (done) => {
-        chai.request(baseUrl)
-            .get('/test/condition?@x=1')
-            .end((err, res) => {
-                res.should.have.status(200);
-                chai.assert.deepEqual(res.body, { "value": 1 });
-                done();
-            });
-    });
+  it('GET condition @x=1', (done) => { // eslint-disable-line
+    chai.request(baseUrl)
+      .get('/test/condition?@x=1')
+      .end((err, res) => {
+        res.should.have.status(200);
+        chai.assert.deepEqual(res.body, { value: 1 });
+        done();
+      });
+  });
 
-    it('GET condition @x=2', (done) => {
-        chai.request(baseUrl)
-            .get('/test/condition?@x=2')
-            .end((err, res) => {
-                res.should.have.status(200);
-                chai.assert.deepEqual(res.body, { "value": 2 });
-                done();
-            });
-    });
+  it('GET condition @x=2', (done) => { // eslint-disable-line
+    chai.request(baseUrl)
+      .get('/test/condition?@x=2')
+      .end((err, res) => {
+        res.should.have.status(200);
+        chai.assert.deepEqual(res.body, { value: 2 });
+        done();
+      });
+  });
 
-    it('GET condition @x=3', (done) => {
-        chai.request(baseUrl)
-            .get('/test/condition?@x=3')
-            .end((err, res) => {
-                res.should.have.status(200);
-                chai.assert.deepEqual(res.body, { "message": "reject" });
-                done();
-            });
-    });
+  it('GET condition @x=3', (done) => { // eslint-disable-line
+    chai.request(baseUrl)
+      .get('/test/condition?@x=3')
+      .end((err, res) => {
+        res.should.have.status(200);
+        chai.assert.deepEqual(res.body, { message: 'reject' });
+        done();
+      });
+  });
 
-    it('simple POST server', (done) => {
-        const body = { "simple-json": true };
+  it('simple POST server', (done) => { // eslint-disable-line
+    const body = { 'simple-json': true };
 
-        chai.request(baseUrl)
-            .post('/test/post')
-            .send(body)
-            .end((err, res) => {
-                res.should.have.status(200);
-                chai.assert.deepEqual(res.body, body);
-                done();
-            });
-    });
+    chai.request(baseUrl)
+      .post('/test/post')
+      .send(body)
+      .end((err, res) => {
+        res.should.have.status(200);
+        chai.assert.deepEqual(res.body, body);
+        done();
+      });
+  });
 });
